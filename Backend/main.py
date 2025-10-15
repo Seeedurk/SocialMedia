883d930fa41b793a7467df68841c5e2061ea3e64
Seeedurk <sedrikpunzalan@gmail.com>
2025-10-15 11:54:21 -0500

    feat
    Added Postgres local database and created the pipeline from frontend to database.
    
    The frontend now creates messages served up from data storted in a Postgres database. Flask now takes new post data from the frontend and sends it straight back to the database. When the server starts up, flask will retreive that database info and send it to frontend to be rendered.

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,20 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
+from flask_sqlalchemy import SQLAlchemy
 import logging
 import json
-import time
+
+#Extremely important commands
+
+#& "C:\Program Files\PostgreSQL\18\bin\psql.exe" -U postgres -p 4000
+#TRUNCATE TABLE message RESTART IDENTITY;
+#db.session.add(Message(user = "Sedrik", text = "Hello from all the way in the database!"))
+#db.session.commit()
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+
 
 users = [
     {"name": "Sedrik",
@@ -20,16 +28,41 @@ Post = [
         },
     {
         "user": "Sedrik",
-        "text": "Send help! Andre keeps touching me!"
+        "text": "Send help! Andre keeps!"
         }
-    ]
+]
+Posts = [{
+        "user": "Sedrik",
+        "text": "If your seeing this, this means very bad things"
+}]
 
-testing = [
-    "I am a thigga message"]
 app = Flask(__name__)
 CORS(app)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:13071418@localhost:4000/testdb'
+db.init_app(app)
+
+
+#Database Modelling
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(50), nullable=False)
+    text = db.Column(db.String(500), nullable=False)
+   
+
+
+def create_database():
+    with app.app_context():
+        db.create_all()
+
+        
+
+#Muh Database Routes
+
+
+#RESTful Routes
 @app.route('/api/message')
 def get_message():
     return jsonify(message="Helo from Flask backend!")
@@ -39,24 +72,36 @@ def recieve_input():
     data = request.get_json()
     return jsonify(status="success", data=data)
 
+
+#SocketIO Routes
 @socketio.on('get_data')
 def retrieve_data(data):
    emit('submit_response', {'message': 'Hello using websockets!'})
 
 @socketio.on('init_posts')
 def handle_post():
-    emit('post_response', json.dumps(Post))
+    Posts = [{"user": message.user, "text": message.text}
+    for message in Message.query.order_by(Message.id.desc()).all()]
+    emit('post_response', json.dumps(Posts))
 
 @socketio.on('new_post')
 def new_post(data):
-    Post.insert(0, data)
-    print(Post)
-    emit('Render_response', json.dumps(Post), broadcast=True)
 
+    print(data)
+    db.session.add(Message(user = data["user"], text = data["text"]))
+    db.session.commit()
+    Posts = [{"user": message.user, "text": message.text}
+    for message in Message.query.order_by(Message.id.desc()).all()]
+    emit('Render_response', json.dumps(Posts), broadcast=True)
 
 
 if __name__ == '__main__':
+    import os
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        create_database()
+        
     socketio.run(app, port=5000, debug=True)
+
 
     
 
--- Client/Components/text.jsx
+++ Client/Components/text.jsx
@@ -5,33 +5,34 @@ import socket from './Socket.js';
 
 function text() {
     const exploded = useRef(false);
-    const [Posts, setPosts] = useState([{
-        user: "Sedrik",
-        text: "Hello, I'm looking for a partner to join me on my adventures. I'm a skilled warrior and a loyal companion. If you're interested, please reach out to me! #Adventurer #PartnerWanted"
-    }
+    const [Posts, setPosts] = useState([
     ]);
 
 
 
     useEffect(() => {
-        
-        socket.emit("init_posts");
-        const Handler = (posts) => {
+
+        const responseHandler = (posts) => {
             if (!exploded.current) {
                 setPosts(prev => [...prev, ...JSON.parse(posts)]);
                 exploded.current = true;
             }
 
         }
-        const Render = (data) => {
+
+        const renderHandler = (data) => {
             setPosts(JSON.parse(data));
             console.log(Posts);
         }
-        socket.on("post_response", Handler);
-        socket.on("Render_response", Render);
+
+        socket.emit("init_posts");
+
+        socket.on("post_response", responseHandler);
+        socket.on("Render_response", renderHandler);
         return () => {
-            socket.off("post_response", Handler);
-            socket.off("Render_response", Render);
+            socket.off("post_response", responseHandler);
+            socket.off("Render_response", renderHandler);
+            
         };
 
     }, [])
@@ -62,7 +63,7 @@ function text() {
             </div>
             <div className="text">
                 <h3>Gavin Mangubat</h3>
-
+                Homsapien
 
             </div>
         
